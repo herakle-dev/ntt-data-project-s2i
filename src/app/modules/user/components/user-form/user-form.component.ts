bbaa360eb19a1a102c76770e1d8411e81ab23348
herakle-dev <ferrigno.ivan98@gmail.com>
2023-06-21 13:14:33 +0200

    fix

--- src/app/modules/user/components/user-form/user-form.component.ts
+++ src/app/modules/user/components/user-form/user-form.component.ts
@@ -1,53 +1,48 @@
 import { Component } from '@angular/core';
 import { UserFormService } from '../../services/user-form.service';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  styleUrls: ['./user-form.component.css'],
 })
-export class UserFormComponent  {
+export class UserFormComponent {
   formData: any = {
     name: '',
     email: '',
     gender: '',
-    status: ''
+    status: '',
   };
 
-  constructor(private userFormService: UserFormService) {
-  }
+  constructor(
+    private userFormService: UserFormService,
+    private router: Router
+  ) {}
   errorCode: number | null = null;
 
   showSidebarForm = false;
   status = false;
 
-
-
   toggleSidebarForm() {
-
-
     this.showSidebarForm = !this.showSidebarForm;
-
   }
 
   submitForm(form: NgForm) {
     this.formData.status = this.status ? 'active' : 'inactive';
-    this.userFormService.createUser(this.formData)
-      .subscribe(
-        () => {
-          alert(`Utente creato con successo!`);
-          location.reload()
-
-        },
-        error => {
-          this.errorCode = error.status;
-        }
-      );
+    this.userFormService.createUser(this.formData).subscribe(
+      () => {
+        alert(`Utente creato con successo!`);
+        location.href='user'
+      },
+      (error) => {
+        this.errorCode = error.status;
+      }
+    );
   }
 
   changeStatus() {
-this.status
-
+    this.status;
   }
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TokenAuthServiceService } from 'src/app/core/shared/token-auth-service.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-constructor(public tokenAuthService:TokenAuthServiceService){}
-userLogout(){
-this.tokenAuthService.deleteToken()
-location.reload()
-}
+  constructor(
+    public tokenAuthService: TokenAuthServiceService,
+    private router: Router
+  ) {}
+  userLogout() {
+    this.tokenAuthService.deleteToken();
+    location.href='login'  }
 }
